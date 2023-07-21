2c91b4c0758aa360e39ce09b0a64e42f713d21af
Yaroslav82 <yargay230607@gmail.com>
2023-07-21 22:43:07 +0300

    Add products adder

--- src/app/task_03/Main.java
+++ src/app/task_03/Main.java
@@ -5,6 +5,11 @@ import java.util.List;
 
 public class Main {
 
+    private static void addProducts(LinkedList<String> list, String firstProduct, String lastProduct) {
+        list.addFirst(firstProduct);
+        list.addLast(lastProduct);
+    }
+
     private static LinkedList<String> getData() {
         return new LinkedList<>(List.of("orange", "grape", "apple", "lemon"));
     }
