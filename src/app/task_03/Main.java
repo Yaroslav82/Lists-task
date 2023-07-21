309a265785fe4df13a3261515048cf4642a94eba
Yaroslav82 <yargay230607@gmail.com>
2023-07-21 22:39:11 +0300

    Add getData method

---
+++ src/app/task_03/Main.java
@@ -0,0 +1,11 @@
+package app.task_03;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+
+    private static LinkedList<String> getData() {
+        return new LinkedList<>(List.of("orange", "grape", "apple", "lemon"));
+    }
+}
