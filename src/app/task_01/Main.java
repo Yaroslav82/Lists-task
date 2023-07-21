7be454eba1b18b8d2a09af3941fbb10d95609534
Yaroslav82 <yargay230607@gmail.com>
2023-07-21 22:23:07 +0300

    Add line break in print

--- src/app/task_01/Main.java
+++ src/app/task_01/Main.java
@@ -31,6 +31,7 @@ public class Main {
             System.out.println(counter + ") " + name);
             counter++;
         }
+        System.out.println();
     }
 
     private static ArrayList<String> getData() {
