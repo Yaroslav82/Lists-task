54e81fb85f22f1a50a0be73436d04ca6c2def099
Yaroslav82 <yargay230607@gmail.com>
2023-07-21 22:35:54 +0300

    Add print methods

--- src/app/task_02/Main.java
+++ src/app/task_02/Main.java
@@ -1,9 +1,29 @@
 package app.task_02;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
+
+    private static void printInitialList(List<String> list) {
+        System.out.println("Initial list:");
+        print(list);
+    }
+
+    private static void printCuttedList(List<String> list) {
+        System.out.println("List part:");
+        print(cutList(list));
+    }
+
+    private static void print(List<String> list) {
+        int counter = 1;
+        for (String name : list) {
+            System.out.println(counter + ") " + name);
+            counter++;
+        }
+        System.out.println();
+    }
 
     private static List<String> cutList(List<String> list) {
         return list.subList(1, 3);
