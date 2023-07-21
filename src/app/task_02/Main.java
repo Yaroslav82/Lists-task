ab5bb7761acc0b2b27be8bd46586b9b10aaccdc6
Yaroslav82 <yargay230607@gmail.com>
2023-07-21 22:28:48 +0300

    Add getData method

---
+++ src/app/task_02/Main.java
@@ -0,0 +1,11 @@
+package app.task_02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    private static ArrayList<String> getData() {
+        return new ArrayList<>(List.of("orange", "mango", "banana", "apple", "kiwi", "cherry"));
+    }
+}
