fcf6e52486b1899125e8ee05d20d1a36778454f3
Yaroslav82 <yargay230607@gmail.com>
2023-07-21 22:06:43 +0300

    Add list printing

--- src/app/task_01/Main.java
+++ src/app/task_01/Main.java
@@ -3,6 +3,15 @@ package app.task_01;
 import java.util.List;
 
 public class Main {
+
+    private static void print(List<String> list) {
+        int counter = 1;
+        for (String name : list) {
+            System.out.println(counter + ") " + name);
+            counter++;
+        }
+    }
+
     private static List<String> getData() {
         return List.of("John", "Alice", "Robert", "Lisa", "David", "Amanda");
     }
