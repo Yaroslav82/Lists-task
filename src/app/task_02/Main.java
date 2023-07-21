b234c5a707ea21255535c469037677790f8b1614
Yaroslav82 <yargay230607@gmail.com>
2023-07-21 22:34:27 +0300

    Add part of list creator

--- src/app/task_02/Main.java
+++ src/app/task_02/Main.java
@@ -5,6 +5,10 @@ import java.util.List;
 
 public class Main {
 
+    private static List<String> cutList(List<String> list) {
+        return list.subList(1, 3);
+    }
+
     private static ArrayList<String> getData() {
         return new ArrayList<>(List.of("orange", "mango", "banana", "apple", "kiwi", "cherry"));
     }
