3ec0d771ce91f0051860c6f24b4c72c884b31c18
Yaroslav82 <yargay230607@gmail.com>
2023-07-21 22:01:56 +0300

    Add getData method

---
+++ src/app/task_01/Main.java
@@ -0,0 +1,9 @@
+package app.task_01;
+
+import java.util.List;
+
+public class Main {
+    private static List<String> getData() {
+        return List.of("John", "Alice", "Robert", "Lisa", "David", "Amanda");
+    }
+}
