300cbf5796120ea3257e618efa9e72aec6654efc
Yaroslav82 <yargay230607@gmail.com>
2023-07-21 22:45:41 +0300

    Add print methods

--- src/app/task_03/Main.java
+++ src/app/task_03/Main.java
@@ -5,6 +5,26 @@ import java.util.List;
 
 public class Main {
 
+    private static void printInitialList(LinkedList<String> list) {
+        System.out.println("Initial list:");
+        print(list);
+    }
+
+    private static void printEditedList(LinkedList<String> list) {
+        System.out.println("Edited list:");
+        addProducts(list, "plum", "mango");
+        print(list);
+    }
+
+    private static void print(List<String> list) {
+        int counter = 1;
+        for (String name : list) {
+            System.out.println(counter + ") " + name);
+            counter++;
+        }
+        System.out.println();
+    }
+
     private static void addProducts(LinkedList<String> list, String firstProduct, String lastProduct) {
         list.addFirst(firstProduct);
         list.addLast(lastProduct);
